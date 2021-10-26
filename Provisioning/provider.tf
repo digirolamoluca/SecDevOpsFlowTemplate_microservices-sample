1676e3d235a392a715e647b9bc4a3a00cbca615f
Luca <90553744+digirolamoluca@users.noreply.github.com>
2021-10-26 17:51:30 +0200

    Update provider.tf

--- Provisioning/provider.tf
+++ Provisioning/provider.tf
@@ -13,7 +13,6 @@ provider "proxmox" {
   
   pm_log_enable = true
   pm_log_file = "terraform-plugin-proxmox.log"
-  pm_debug = true
   pm_log_levels = {
     _default = "debug"
     _capturelog = ""
