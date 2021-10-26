8b6406fd73e0fbafcd3b38018bd212f8c5d19777
Luca <90553744+digirolamoluca@users.noreply.github.com>
2021-10-26 18:00:59 +0200

    Update provider.tf

--- Provisioning/provider.tf
+++ Provisioning/provider.tf
@@ -10,11 +10,12 @@ provider "proxmox" {
     pm_api_url = "https://10.224.16.41:8006/api2/json"
     pm_password = "xbXif4P82Q88iA=="
     pm_tls_insecure = "true"
-  
+  /*
   pm_log_enable = true
   pm_log_file = "terraform-plugin-proxmox.log"
   pm_log_levels = {
     _default = "debug"
     _capturelog = ""
+    */
   }
 }
